7ac6e885068281c3be7f179c63dfe2ea70513f4b
Cássia Sampaio <4005687+cassiasamp@users.noreply.github.com>
2025-03-18 15:41:46 -0300

    update and rename .eslintrc.json to .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+{
+  "extends": "next/core-web-vitals"
+}
+module.exports = {
+  // other configurations
+  linterOptions: {
+    reportUnusedDisableDirectives: true
+  },
+  // other configurations
+};
